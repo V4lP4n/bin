5cb03de85c818874cccfbd802badbdc10a103c74
v <sepure@protonmail.com>
2021-03-13 14:24:02 +0300

    *

---
+++ sh/nvim.sh
@@ -0,0 +1,17 @@
+sudo apt-get install fuse libfuse2 git python3-pip ack-grep -y
+wget --quiet https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage --output-document nvim
+chmod +x nvim
+sudo chown root:root nvim
+sudo mv nvim /usr/bin
+cd ~
+mkdir -p .config/nvim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+pip3 install --user neovim
+touch ~/.config/nvim/init.vim
+echo -e"call plug#begin()\n\
+Plug 'roxma/nvim-completion-manager'\n\
+Plug 'SirVer/ultisnips'\n\
+Plug 'honza/vim-snippets'\n\
+call plug#end()"
+nvim
+
